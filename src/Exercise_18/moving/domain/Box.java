1daa6d6ad5679384c056923b9029c270f070bb93
Dmytro Politov <dmytropolitov@gmail.com>
2017-01-12 22:01:59 -0500

    Initial commit: Two classes implement interface to allow them to be grouped into collections. Packer class defines size of boxes and adds lists of Items to Boxes to produce a list of boxes. An interesting application of storing a indefinite list of objects of various sizes into groups which are then stored in another list of indefinite size. An interative approach that works on lists of items of size n so long as no object in the list is larger than the maximum size of the grouping.

---
+++ src/Exercise_18/moving/domain/Box.java
@@ -0,0 +1,36 @@
+package Exercise_18.moving.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Exercise 18: Moving
+ *
+ * Object-Oriented Programming with Java Part 2 : 2017-01-12
+ */
+
+public class Box implements Thing{
+    private int currentVolume;
+    private final int maximumCapacity;
+    private List<Thing> items;
+
+    public Box(int maximumCapacity){
+        this.maximumCapacity = maximumCapacity;
+        this.currentVolume = 0;
+        this.items = new ArrayList<Thing>();
+    }
+
+    public boolean addThing(Thing thing){
+        if (this.getVolume() + thing.getVolume() <= this.maximumCapacity){
+            items.add(thing);
+            currentVolume += thing.getVolume();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int getVolume(){
+        return this.currentVolume;
+    }
+}
