d935451c34678f99734139c98a737f97c91e270e
Dmytro Politov <dmytropolitov@gmail.com>
2017-01-12 19:47:23 -0500

    Initial commit: exploration of packages and visibility within packages through two simple classes.

---
+++ src/mooc/ui/TextUserInterface.java
@@ -0,0 +1,16 @@
+package mooc.ui;
+
+/**
+ * Exercise 17: First Packages
+ *
+ * Object-Oriented Programming with Java Part 2 : 2017-01-10
+ */
+public class TextUserInterface implements UserInterface{
+    public TextUserInterface(){
+
+    }
+
+    public void update(){
+        System.out.println("Updating the user interface");
+    }
+}
