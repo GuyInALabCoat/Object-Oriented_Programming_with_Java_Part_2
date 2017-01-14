19568b0099331dfdd1115cd77cbcf48526686e96
Dmytro Politov <dmytropolitov@gmail.com>
2017-01-13 21:07:30 -0500

    Initial commit: An extended and more powerful dictionary that stores translations as sets for each key in the dictionary. Allows for multiple entries to be stored for each word. If key is not found in dictionary, it simply returns a null reference.

---
+++ src/Exercise_24/dictionary/MultipleEntryDictionary.java
@@ -0,0 +1,15 @@
+package Exercise_24.dictionary;
+
+import java.util.Set;
+
+/**
+ * Exercise 24: Multiple Entry Dictionary
+ *
+ * Object-Oriented Programming with Java Part 2 : 2017-01-13
+ */
+
+public interface MultipleEntryDictionary {
+    void add(String word, String translation);
+    Set<String> translate(String word);
+    void remove(String word);
+}
