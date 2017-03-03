b6c5f8686f11e478bcdf4db87fe82f2da181225a
Dmytro Politov <dmytropolitov@gmail.com>
2017-03-02 19:08:47 -0500

    Added comments to clarify function.

--- src/Week_11/Exercise_39/clicker/ui/ClickerListener.java
+++ src/Week_11/Exercise_39/clicker/ui/ClickerListener.java
@@ -12,18 +12,19 @@ import java.awt.event.ActionListener;
  * Object-Oriented Programming with Java Part 2 : 2017-03-02
  */
 
+// Action listener object that increments counter upon click
 public class ClickerListener implements ActionListener{
     private Calculator calculator;
     private JLabel label;
 
     public ClickerListener(Calculator calculator, JLabel label){
-        this.calculator = calculator;
-        this.label = label;
+        this.calculator = calculator;                               // reference to calculator that holds current value
+        this.label = label;                                         // reference to text label in user interface
     }
 
     @Override
     public void actionPerformed(ActionEvent ae){
-        this.calculator.increase();
-        label.setText(Integer.toString(this.calculator.giveValue()));
+        this.calculator.increase();                                 // upon click, increment the counter
+        label.setText(Integer.toString(this.calculator.giveValue()));   // update the text label to display the new incremented value
     }
 }
